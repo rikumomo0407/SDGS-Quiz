ba4fd1b0dd36a2ea4dae1c589197f3751b65a7c1
rikumomo0407 <92307653+rikumomo0407@users.noreply.github.com>
2023-01-31 13:17:15 +0900

    main_en.py を更新

--- main_en.py
+++ main_en.py
@@ -1,24 +1,24 @@
 """
 -----------------------------------------------
-(解答)
+(Answers)
 -----------------------------------------------
-1.貧困をなくそう
-2.飢餓をゼロに
-3.すべての人に健康と福祉を
-4.質の高い教育をみんなに
-5.ジェンダー平等を実現しよう
-6.安全な水とトイレを世界中に
-7.エネルギーをみんなにそしてクリーンに
-8.働きがいも経済成長も
-9.産業と技術革新の基盤をつくろう
-10.人や国の不平等をなくそう
-11.住み続けられるまちづくりを
-12.つくる責任つかう責任
-13.気候変動に具体的な対策を
-14.海の豊かさを守ろう
-15.陸の豊かさもまもろう
-16.平和と公正をすべての人に
-17.パートナーシップで目標を達成しよう
+1.No Poverty
+2.Zero Hunger
+3.Good Health and Well-Being
+4.Quality Education
+5.Gender Equality
+6.Clean Water and Sanitation
+7.Affordable and Clean Energy
+8.Decent Work and Economic Growth
+9.Industry, Innovation and Infrastructure
+10.Reduced Inequalities
+11.Sustainable Cities and Communities
+12.Responsible Consumption and Production
+13.Climate Action
+14.Life Below Water
+15.Life On Land
+16.Peace, Justice and Strong Institutions
+17.Partnerships for the Goals
 -----------------------------------------------
 """
 
@@ -51,7 +51,7 @@ path = [
 finished = []
 correct = 0
 for i in range(17):
-    print("\r//////////問題" + str(len(finished) + 1) + "//////////")
+    print("\r//////////Question" + str(len(finished) + 1) + "//////////")
     while(True):
         order = random.randrange(17)
         if (order in finished) == 0:
@@ -62,22 +62,22 @@ for i in range(17):
     cv2.waitKey()
     cv2.destroyAllWindows()
     while(True):
-        print("答えを入力 >>> " ,end="")
+        print("Input correct number >>> " ,end="")
         ans = input()
         if ans.isdecimal() == False:
-            print("***1~17数字を入力してください***")
+            print("***Input number range 1~17***")
         elif int(ans) < 0 or 18 < int(ans):
-            print("***1~17の範囲で入力してください***")
+            print("***Input number range 1~17***")
         else:
             break
     if int(ans) - 1 == order:
-        print("正解!!")
+        print("Correct!!")
         correct += 1
     else:
-        print("不正解 (答え: " + str(order + 1) + ")")
+        print("Incorrect (Answer: " + str(order + 1) + ")")
     if i < 16:
         for i in reversed(range(4)):
             time.sleep(1)
-            print("\r次の問題まで: " + str(i),end="")
-print("//////////結果//////////")
-print("正解率:" + str(correct) +"/17")
+            print("\rNext question: " + str(i),end="")
+print("//////////Result//////////")
+print("Accuracy rate:" + str(correct) +"/17")
